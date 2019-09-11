e9e771546833e9414dbfb743e8b30cc4dfb85d7e
Pascal Precht <pascal.precht@gmail.com>
2019-09-11 15:29:14 +0200

    lesson 6: Simple error handling with expect()
    * Reads user input to get two numbers for `sum()`
    * Uses `.parse()` to parse `String` to `u32` for each input
    * Unwraps parse `Result` using `expect()` to provide better error message
    
    To run this:
    
    ```
    $ cd sum
    $ cargo run
    ```

--- src/sum/src/main.rs
+++ src/sum/src/main.rs
@@ -1,5 +1,21 @@
+use std::io;
+
 fn main() {
-  let result = sum(3, 7);
+  println!("Please enter the first number: ");
+
+  let mut first = String::new();
+  io::stdin().read_line(&mut first).unwrap();
+
+  let a: u32 = first.trim().parse().expect("Not a number");
+
+  println!("Please enter a second number: ");
+
+  let mut second = String::new();
+  io::stdin().read_line(&mut second).unwrap();
+
+  let b: u32 = second.trim().parse().expect("Not a number");
+
+  let result = sum(a, b);
   println!("Result: {}", result);
 }
 
