53f27b4545c5fb22c4bd530f0a17e286cd9b7752
Pascal Precht <pascal.precht@gmail.com>
2019-09-11 15:53:31 +0200

    lesson 9: Keep asking
    * Introduces `loop` to keep asking the user for input
    
    To run this:
    
    ```
    $ cd sum
    $ cargo run
    ```

--- src/sum/src/main.rs
+++ src/sum/src/main.rs
@@ -2,38 +2,40 @@ use std::io;
 use std::process;
 
 fn main() {
-  println!("Please enter the first number: ");
+  loop {
+    println!("Please enter the first number: ");
 
-  let mut first = String::new();
-  io::stdin().read_line(&mut first).unwrap();
+    let mut first = String::new();
+    io::stdin().read_line(&mut first).unwrap();
 
-  let a: u32;
+    let a: u32;
 
-  match first.trim().parse() {
-    Ok(val) => a = val,
-    Err(_err) => {
-      println!("Not a number!");
-      process::exit(1)
-    }
-  };
+    match first.trim().parse() {
+      Ok(val) => a = val,
+      Err(_err) => {
+        println!("Not a number!");
+        process::exit(1)
+      }
+    };
 
-  println!("Please enter a second number: ");
+    println!("Please enter a second number: ");
 
-  let mut second = String::new();
-  io::stdin().read_line(&mut second).unwrap();
+    let mut second = String::new();
+    io::stdin().read_line(&mut second).unwrap();
 
-  let b: u32;
+    let b: u32;
 
-  match second.trim().parse() {
-    Ok(val) => b = val,
-    Err(_err) => {
-      println!("Not a number!");
-      process::exit(1)
-    }
-  };
+    match second.trim().parse() {
+      Ok(val) => b = val,
+      Err(_err) => {
+        println!("Not a number!");
+        process::exit(1)
+      }
+    };
 
-  let result = sum(a, b);
-  println!("Result: {}", result);
+    let result = sum(a, b);
+    println!("Result: {}", result);
+  }
 }
 
 fn sum(a: u32, b: u32) -> u32 {
