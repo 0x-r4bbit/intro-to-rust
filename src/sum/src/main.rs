45ae3c5ed42c136c5f1b6dc03e1043d38c9eaa32
Pascal Precht <pascal.precht@gmail.com>
2019-09-11 15:39:57 +0200

    lesson 7: Error handing using Pattern Matching
    * Uses `match` statement to parse both user input values
    * Stores result of each input when parsing was successful, outputs error message if not
    
    To run this:
    
    ```
    $ cd sum
    $ cargo run
    ```

--- src/sum/src/main.rs
+++ src/sum/src/main.rs
@@ -6,14 +6,24 @@ fn main() {
   let mut first = String::new();
   io::stdin().read_line(&mut first).unwrap();
 
-  let a: u32 = first.trim().parse().expect("Not a number");
+  let mut a: u32 = 0;
+
+  match first.trim().parse() {
+    Ok(val) => a = val,
+    Err(_err) => println!("Not a number!")
+  };
 
   println!("Please enter a second number: ");
 
   let mut second = String::new();
   io::stdin().read_line(&mut second).unwrap();
 
-  let b: u32 = second.trim().parse().expect("Not a number");
+  let mut b: u32 = 0;
+
+  match second.trim().parse() {
+    Ok(val) => b = val,
+    Err(_err) => println!("Not a number!")
+  };
 
   let result = sum(a, b);
   println!("Result: {}", result);
